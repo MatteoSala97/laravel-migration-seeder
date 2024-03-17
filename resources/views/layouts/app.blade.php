22ec66dcb8bc6a8d1cdada71f7c30dfc03467ffc
Matteo Sala <matteo111997@gmail.com>
2024-03-17 16:43:16 +0100

    sections creation and main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laravel migration seeder</title>
+    <title>
+        @yield('title')
+    </title>
 
     @vite('resources/js/app.js')
-    
+
 </head>
 <body>
 
+    @yield('header')
+
+    @yield('main')
 </body>
 </html>
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', "Trains | Home")
+
+@section('main')
+
+<main>
+    <h1 class="text-center">
+       Trains
+    </h1>
+</main>
+
+@endsection
